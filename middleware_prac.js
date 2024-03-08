2de4efa00b85a1891cded4ba58b656a079a15605
bhanu mukkamala <bmukkamala68@gmail.com>
2024-03-08 22:10:28 +0530

    Added another middleware to calculate the duration of the response

--- middleware_prac.js
+++ middleware_prac.js
@@ -1,40 +1,50 @@
-const express = require("express");
-const app = express();
-let noOfRequest = 0;
-//middleware to calculate the number of times a client has hit your server 
-const calculateRequest = (req,res,next) => {
-  noOfRequest+=1;
-  console.log(noOfRequest);
-  next();
-}
-app.use(calculateRequest);
+    const express = require("express");
+    const app = express();
+    let noOfRequest = 0;
+    //middleware to calculate the number of times a client has hit your server 
+    const calculateRequest = (req,res,next) => {
+      req.startTime= Date.now();
+      noOfRequest+=1;
+      console.log(noOfRequest);
+      next();
+    }
+    app.use(calculateRequest);
 
 
-app.get("/health-checkup",(req,res)=>{
-  const kidneyId = req.query.kidneyId;
-  const username = req.headers.username;
-  const password = req.headers.password;
+    app.get("/health-checkup",(req,res)=>{
+      const kidneyId = req.query.kidneyId;
+      const username = req.headers.username;
+      const password = req.headers.password;
+    //calculating the duration of the requests    
+    try{
+      if(username != "bhanu" || password != "pass" ){
+        throw new Error("Wrong Details");
+      }
+      if(kidneyId != 1 && kidneyId !=2){
+        throw new Error("Invalid Inputs!");
+    }
+    res.status(200).json({
+      msg:"Chill, your kidneys are fineee"
+    })
+    }
+    catch(error){
+      if(error instanceof Error){
+        const statusCode = error.message === "Wrong Details" ? 400: 403;
+        res.status(statusCode).json({
+          msg: error.message
+        })
+        return;
+      }
+    }
+    finally{
+    req.duration= Date.now()-req.startTime;
+      const durationInSec = req.duration/1000;
+      
+      const formattedDuration = `${Math.floor(durationInSec)} seconds ${req.duration % 1000} milliseconds`;
+      console.log(`${formattedDuration} is the average time taken by the server to respond`);
 
+    
+    }
+    })
 
- if(username != "bhanu" || password != "pass" ){
-  res.status(400).json({
-    msg:"Wrong details"
-  })
-  return;
- }
- if(kidneyId != 1 && kidneyId !=2){
-  res.status(403).json({
-    msg:"Something's Up with your inputs!",
-  })
-  return;
- }
- res.status(200).json({
-  msg:"Chill, your kidneys are fineee"
- })
-
-
-})
-
-
-
-app.listen(3001);
+    app.listen(3001);
